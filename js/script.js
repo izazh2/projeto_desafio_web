c6f7615862ff1ecc42ea49db5463601f30d55929
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-06-09 21:47:52 -0300

    criado o js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("Teste")
+
+//Pegando elementos do dom
+const formPessoa = document.querySelector("formulario-pessoa")
+const divDados = document.querySelector("#div-dados")
+
+formPessoa.addEventListener('submit',(evt)=>{
+    evt.preventDefault()
+
+    const dadosForm = new FormData(formPessoa)
+
+    const nome = dadosForm.get('nome')
+    const idade = dadosForm.get('idade')
+
+    let situacaoIdade = idade >=  18 ? ` ${nome}, é Maior de idade` : `${nome},é Menor de idade`
+
+    divDados.innerHTML = situacaoIdade
+})
